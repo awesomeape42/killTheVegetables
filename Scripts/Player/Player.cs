e30a8f5b7ca3407b1cb10971a0f4bc2d624d05f7
awesomeape42 <antekpo123@gmail.com>
2024-07-09 20:54:01 +0100

    modified:   Scripts/Player/Player.cs 	modified:   player.tscn 	modified:   project.godot

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -4,7 +4,12 @@ namespace killTheVegetables.scripts.Player;
 
 public partial class Player : RigidBody3D
 {
-    [Export] private float movementSpeed = 1f;
+    [Export] private float movementSpeedGround = 15f;
+    [Export] private float movementSpeedAir = 5f;
+
+    [Export] private float dampingGround = 4f;
+    [Export] private float dampingAir = 0f;
+    
     [Export] private float jumpStrength = 30f;
 	
     private Camera camera;
@@ -15,12 +20,41 @@ public partial class Player : RigidBody3D
         camera = GetNode<Camera>("Camera");
         groundRaycast = GetNode<RayCast3D>("GroundRaycast");
     }
-
-    private bool justJumped = false;
+    
     private bool isGrounded = false;
+    private bool justJumped = false;
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
+        if (!groundRaycast.IsColliding())
+        {
+            justJumped = false;
+            isGrounded = false;
+        }
+        else if (!justJumped)
+        {
+            isGrounded = true;
+        }
+
+        float speed;
+        if (isGrounded)
+        {
+            speed = movementSpeedGround;
+            LinearDamp = dampingGround;
+        }
+        else
+        {
+            speed = movementSpeedAir;
+            LinearDamp = dampingAir;
+        }
+
+        if (Input.IsActionPressed("jump") && isGrounded)
+        {
+            ApplyCentralImpulse(Vector3.Up * jumpStrength);
+            justJumped = true;
+            isGrounded = false;
+        }
+        
         var rawInputDirection = Vector3.Zero;
 
         if (Input.IsActionPressed("moveForwards"))
@@ -33,23 +67,6 @@ public partial class Player : RigidBody3D
             rawInputDirection.X += 1f;
 
         var movementDirection = rawInputDirection.Rotated(Vector3.Up, Mathf.DegToRad(camera.yaw)).Normalized();
-        ApplyCentralForce(movementDirection * movementSpeed);
-
-        if (!groundRaycast.IsColliding())
-        {
-            justJumped = false;
-            isGrounded = false;
-        }
-        else if (!justJumped)
-        {
-            isGrounded = true;
-        }
-
-        if (Input.IsActionPressed("jump") && isGrounded)
-        {
-            ApplyCentralImpulse(Vector3.Up * jumpStrength);
-            justJumped = true;
-            isGrounded = false;
-        }
+        ApplyCentralForce(movementDirection * speed * (float)delta * 100f);
     }
 }
